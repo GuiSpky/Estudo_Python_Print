4e5d71a21fa9b30533dcebce7eb3fb5d5134c0f6
Guilherme Fernandes de Souza <104801386+GuiSpky@users.noreply.github.com>
2024-03-02 08:47:44 -0300

    Add files via upload

---
+++ aula8_exc.py
@@ -0,0 +1,17 @@
+# Estudo na utilização de print e fString, análise de 
+# diferentes métodos para dicionar variáveils em print
+
+
+nome = 'Guilherme' 
+sobrenome = 'Fernandes de Souza'
+idade = 21
+ano_nascimento = 2022 - idade
+maior_de_idade = idade >= 18
+altura = 1.78
+
+print(f'Nome: {nome}')
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura:', altura)
